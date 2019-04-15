07ac477656cc9aff84fb421a551df6d3c7bf8a39
Student <Student@CloAut0973.dir.svc.accenture.com>
2019-04-15 15:55:44 +0300

    New feature - Guess the number (some fix)

--- janiss/guess_numbers.py
+++ janiss/guess_numbers.py
@@ -4,7 +4,7 @@ secret = random.randint(1,10)
 #print(secret)
 won = False
 while won != True:
-    number=input("Guess a number between 1 and 10:")
+    number=input("Guess a number between 1 and 10: ")
     if int(number) > secret :
         print("Too High")
     elif int(number) < secret :
