26a257cb99e4bcb5c8383a80339d0a227bfda966
Student <Student@CloAut0973.dir.svc.accenture.com>
2019-04-15 14:59:11 +0300

    Turtle (draw a colored rectangle)

--- janiss/t_turtle.py
+++ janiss/t_turtle.py
@@ -1,8 +1,6 @@
 import turtle
-for steps in range(4):
+for steps in ['red','blue','green','black']:
+    turtle.color(steps)
     turtle.forward(100)
     turtle.right(90)
-    for moresteps in range(4):
-        turtle.forward(50)
-        turtle.right(90)
 turtle.done()
