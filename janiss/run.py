7d4b4334f1152607dcbcfaa86bf359babc4d8899
Student <Student@CloAut0973.dir.svc.accenture.com>
2019-04-15 13:26:47 +0300

    New feature -> Choices to select (fixed bug)

--- janiss/run.py
+++ janiss/run.py
@@ -3,7 +3,7 @@
 import os
 actions = {"action 2":"practice with Sdx","action 3":"sleep","action 1":"read book"}
 while True:
-    os.system("cls")
+    #os.system("cls")
     #print("Python is a programming language\nthat lets yu work quickly\nand integrate systems more effectively\n")
     name = raw_input ("What's your name? ")
     print("Hello, " + name)
@@ -13,4 +13,4 @@ while True:
         print(name,value)
     action_rawinput = raw_input("Your choose : ")
     action_input = "action "+str(action_rawinput)
-    print("I'll {} later".format(actions.get(action_input))
+    print("I'll {} later".format(actions.get(action_input)))
