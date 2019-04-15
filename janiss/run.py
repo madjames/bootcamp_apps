aff9fab1df1d9c70a8667452f4521fc62b6842f2
Student <Student@CloAut0973.dir.svc.accenture.com>
2019-04-15 11:17:47 +0300

    Testing Python newliners

--- janiss/run.py
+++ janiss/run.py
@@ -1,3 +1,6 @@
 # My first Python app
 # Cool ! Isn't it?
-print("Hello World!")
+import time
+while True:
+    print("Python is a programming language\nthat lets you work quickly\nand integrate systems more effectively\n")
+    time.sleep(3)
