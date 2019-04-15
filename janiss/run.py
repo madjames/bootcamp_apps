d76e4e4df89116539a718e49b9724347c68c5fef
Student <Student@CloAut0973.dir.svc.accenture.com>
2019-04-15 14:10:41 +0300

    New feature -> Clear console

--- janiss/run.py
+++ janiss/run.py
@@ -1,9 +1,13 @@
 # My first Python app
 # Cool ! Isn't it?
 import os
+import platform
 actions = {"action 2":"practice with Sdx","action 3":"sleep","action 1":"read book"}
 while True:
-    #os.system("cls")
+    if (platform.system() == 'Windows'):
+        os.system("cls")
+    else:
+        os.system("clear")
     #print("Python is a programming language\nthat lets yu work quickly\nand integrate systems more effectively\n")
     name = raw_input ("What's your name? ")
     print("Hello, " + name)
