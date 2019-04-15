21de7537a176750862952cdc7350d2b1de8f434b
Student <Student@CloAut0973.dir.svc.accenture.com>
2019-04-15 11:36:00 +0300

    Testing Python input

--- janiss/run.py
+++ janiss/run.py
@@ -2,5 +2,7 @@
 # Cool ! Isn't it?
 import time
 while True:
-    print("Python is a programming language\nthat lets you work quickly\nand integrate systems more effectively\n")
-    time.sleep(3)
+    #print("Python is a programming language\nthat lets you work quickly\nand integrate systems more effectively\n")
+    name = raw_input ("what's your name? ")
+    print("Hello, " + name)
+    time.sleep(1)
