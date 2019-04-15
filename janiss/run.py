08f03b76582462c3813ac0ebe34bfc2c0bd7a0b3
Student <Student@CloAut0973.dir.svc.accenture.com>
2019-04-15 13:24:42 +0300

    New feature -> Choices to select

--- janiss/run.py
+++ janiss/run.py
@@ -1,8 +1,16 @@
 # My first Python app
 # Cool ! Isn't it?
-import time
+import os
+actions = {"action 2":"practice with Sdx","action 3":"sleep","action 1":"read book"}
 while True:
-    #print("Python is a programming language\nthat lets you work quickly\nand integrate systems more effectively\n")
-    name = raw_input ("what's your name? ")
+    os.system("cls")
+    #print("Python is a programming language\nthat lets yu work quickly\nand integrate systems more effectively\n")
+    name = raw_input ("What's your name? ")
     print("Hello, " + name)
-    time.sleep(1)
+    print(actions)
+    print("Please choose your action :")
+    for name, value in actions.items():
+        print(name,value)
+    action_rawinput = raw_input("Your choose : ")
+    action_input = "action "+str(action_rawinput)
+    print("I'll {} later".format(actions.get(action_input))
