9d29784a352f140855d8d0956ac46750e827fe2b
Student <Student@CloAut0973.dir.svc.accenture.com>
2019-04-16 15:08:10 +0300

    New Python script : Flask REST API : client - server (PING fixed idents)

--- janiss/py_flask_rest_server.py
+++ janiss/py_flask_rest_server.py
@@ -5,49 +5,45 @@ from subprocess import Popen, PIPE
 class Ping(Resource):
 	def get(self,ip):		
 		process = Popen(['ping',ip,'-n','2'], shell=True, stdout=PIPE, stderr=PIPE)
-		#print (process.stdout.read())
 		out = proccess.stdou.read()
-		#print("exit code: {}".format(rc))	
-		#print("error: {}".format(process.stderr.read()))
 		return out
 		
 class TodoList(Resource):
-
 	def get(self):
-        print("debug: sending fill list")
-        return TODOS
+		print("debug: sending fill list")
+		return TODOS
 
-    def delete(self,todo_id):
-        print("debug: deleting task with id {}".format(todo_id))
-        #abort_if_doenst_exist
-        del TODOS[todo_id] #deletes a dictionary
-        return TODOS[todo_id]
+	def delete(self,todo_id):
+		print("debug: deleting task with id {}".format(todo_id))
+		#abort_if_doenst_exist
+		del TODOS[todo_id] #deletes a dictionary
+		return TODOS[todo_id]
 
-    def post(self):
-        args = parser.parse_args()
-        todo_id = int(max(TODOS.keys()).lstrip('todo'))+1
-        todo_id = 'todo%i' % todo_id
-        TODOS[todo_id]={'task':args['task']}
-        print("debug: added task with id {}".format(todo_id))
-        return TODOS[todo_id], 201
-    
-    def put(self,todo_id):
-        args = parser.parse_args()
-        task = {'task': args['task']}
-        TODOS[todo_id] = task
-        return task, 201
+	def post(self):
+		args = parser.parse_args()
+		todo_id = int(max(TODOS.keys()).lstrip('todo'))+1
+		todo_id = 'todo%i' % todo_id
+		TODOS[todo_id]={'task':args['task']}
+		print("debug: added task with id {}".format(todo_id))
+		return TODOS[todo_id], 201
+
+	def put(self,todo_id):
+		args = parser.parse_args()
+		task = {'task': args['task']}
+		TODOS[todo_id] = task
+		return task, 201
 
 class Todo(Resource):
-    def get(self, todo_id):
-       #abort_if_todo_doesnt(todo_id)
-        return TODOS[todo_id]
+	def get(self, todo_id):
+		#abort_if_todo_doesnt(todo_id)
+		return TODOS[todo_id]
 
 # Creates a dictionary with content ..
 TODOS = {
-        'todo1':{'task':'build an API'},
-        'todo2':{'task':'rest a bit'},
-        'todo3':{'task':'do some workout'}
-        }
+'todo1':{'task':'build an API'},
+'todo2':{'task':'rest a bit'},
+'todo3':{'task':'do some workout'}
+}
 
 app = Flask(__name__)
 api = Api(app)
@@ -63,4 +59,4 @@ api.add_resource(Ping,'/ping/<ip>')
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+	app.run(debug=True, host='0.0.0.0')
