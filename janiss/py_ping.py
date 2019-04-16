2db7088384ae6d266218fed46f77f2d36c062ad5
Student <Student@CloAut0973.dir.svc.accenture.com>
2019-04-16 09:42:19 +0300

    New Python script : Ping an IP address

---
+++ janiss/py_ping.py
@@ -0,0 +1,9 @@
+from subprocess import Popen, PIPE
+
+process = Popen(['ping','8.8.1.8','-n','2'], shell=True, stdout=PIPE, stderr=PIPE)
+
+print (process.stdout.read())
+rc = process.wait()
+print("exit code: {}".format(rc))
+
+print("error: {}".format(process.stderr.read()))
