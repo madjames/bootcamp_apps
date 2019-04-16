151d14745a747b41010e7bd88d5142be9a1e93fd
Student <Student@CloAut0973.dir.svc.accenture.com>
2019-04-16 14:31:23 +0300

    Flask RESTFUL client - server v.4.0 (Ping option)

--- janiss/py_flask_rest_server.py
+++ janiss/py_flask_rest_server.py
@@ -1,9 +1,19 @@
 from flask import Flask
 from flask_restful import reqparse, abort, Resource, Api
+from subprocess import Popen, PIPE
 
-
+class Ping(Resource):
+	def get(self,ip):		
+		process = Popen(['ping',ip,'-n','2'], shell=True, stdout=PIPE, stderr=PIPE)
+		#print (process.stdout.read())
+		out = proccess.stdou.read()
+		#print("exit code: {}".format(rc))	
+		#print("error: {}".format(process.stderr.read()))
+		return out
+		
 class TodoList(Resource):
-    def get(self):
+
+	def get(self):
         print("debug: sending fill list")
         return TODOS
 
@@ -35,7 +45,8 @@ class Todo(Resource):
 # Creates a dictionary with content ..
 TODOS = {
         'todo1':{'task':'build an API'},
-        'todo2':{'task':'?????'},
+        'todo2':{'task':'rest a bit'},
+        'todo3':{'task':'do some workout'}
         }
 
 app = Flask(__name__)
@@ -48,6 +59,8 @@ parser.add_argument('task')
 # Adding handlers to an App
 api.add_resource(TodoList,'/todos')
 api.add_resource(Todo,'/todos/<todo_id>')
+api.add_resource(Ping,'/ping/<ip>')
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
--- janiss/py_ping.py
+++ janiss/py_ping.py
@@ -1,6 +1,6 @@
 from subprocess import Popen, PIPE
 
-process = Popen(['ping','8.8.1.8','-n','2'], shell=True, stdout=PIPE, stderr=PIPE)
+process = Popen(['ping','8.8.8.8','-n','2'], shell=True, stdout=PIPE, stderr=PIPE)
 
 print (process.stdout.read())
 rc = process.wait()
