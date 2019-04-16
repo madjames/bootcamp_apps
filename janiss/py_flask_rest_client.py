ef26c6e79aeb8569b1ead98bad50b867509e3e7d
Student <Student@CloAut0973.dir.svc.accenture.com>
2019-04-16 12:07:01 +0300

    Flask REST API client - server

--- janiss/py_flask_rest_client.py
+++ janiss/py_flask_rest_client.py
@@ -1,12 +1,8 @@
 from requests import put,get
-ret = put('http://localhost:5000/todo1',data={'data':'Remember the milk'})
-print("PUT(todo1): {}".format(ret.json()))
 
-ret = get('httpL//localhost:5000/todo1')
-print("GET(todo1): {}".format(ret.json()))
+ret = get('http://localhost:5000/todos')
+print("GET({}): ".format(ret.status_code,ret.json()))
 
-ret = put('http://localhost:5000/todo2',data={'data':'Change my breakepads'})
-print("PUT(todo2): {}".format(ret.json()))
+ret = post('http://localhost:5000/todos',data={'task':'cool task'})
+print("POST({}): ".format(ret.status_code,ret.json()))
 
-ret = get('http://localhost:5000/todo2')
-print("GET(todo2): {}".format(ret.json()))
--- janiss/py_flask_rest_server.py
+++ janiss/py_flask_rest_server.py
@@ -1,20 +1,33 @@
 from flask import Flask
-from flask_restful import Resource, Api
+from flask_restful import reqparse, abort, Resource, Api
+
+
+class TodoList(Resource):
+    def get(self):
+        print("debug: sending fill list")
+        return TODOS
+
+    def post(self):
+        args = parser.parse_args()
+        todo_id = int(max(TODOS.keys()).lstrip('todo'))+1
+        todo_id = 'todo%i' % todo_id
+        TODOS[todo_id]={'task':args['task']}
+        print("debug: added task with id {}".format(todo_id))
+        return TODOS[todo_id], 201
+
+TODOS = {
+        'todo1':{'task':'build an API'},
+        'todo2':{'task':'?????'},
+        }
+
 
 app = Flask(__name__)
 api = Api(app)
 
-todos= ()
+parser = reqparse.RequestParser()
+parser.add_argument('task')
 
-class TodoSimple(Resource):
-    def get(self,todo_id):
-        return {todo_id: todos[todo_id]}
-
-    def put(self,todo_id):
-        todos[todo_id] = request.form['data']
-        return {todo_id: todos[todo_id]}
-
-api.add_resource(TodoSimple,'/<string:todo_id>')
+api.add_resource(TodoList,'/todos')
 
 if __name__ == '__main__':
     app.run(debug=True)
